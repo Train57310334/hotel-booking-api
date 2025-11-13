ac70d85365836400e1b53bd2e72f38e5e00917ac
Itthiphon <itthiphonseeso@gmail.com>
2025-11-14 00:27:51 +0700

    fix search module

--- src/modules/search/search.module.ts
+++ src/modules/search/search.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SearchController } from './search.controller';
+import { SearchService } from './search.service';
 
 @Module({
   controllers: [SearchController],
+  providers: [SearchService], // ✅ ต้องมี
+  exports: [SearchService],   // (ถ้าใช้จาก module อื่น)
 })
 export class SearchModule {}
