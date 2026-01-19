2ab70400d8ac1469b82d094aca8f537f09c17424
Itthiphon <itthiphonseeso@gmail.com>
2026-01-19 20:47:38 +0700

    update api

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors({ origin: true, credentials: true });
 
